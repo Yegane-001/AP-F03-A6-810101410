c766283184ddb7c64859bf052a41333db9f5ca60
Yegane-001 <yeganeheidari435@gmail.com>
2024-12-16 23:00:37 +0330

    signup command handled

--- Utaste.cpp
+++ Utaste.cpp
@@ -1,25 +1,54 @@
 #include "Utaste.h"
 #include <iostream>
 #include <sstream>
+using namespace std;
 const string GET = "GET";
 const string PUT = "PUT";
 const string DELETE = "DELETE";
 const string POST = "POST";
 
 void Utaste::handle_input() {
-    string command;
-    while(getline(cin,command)) {
-    if(command.find(GET)==0)
+    string method;
+    while(getline(cin,method)) {
+    if(method.find(GET)==0)
         handle_get();
-    else if(command.find(PUT)==0)
+    else if(method.find(PUT)==0)
        handle_put();
-    else if(command.find(DELETE)==0)
+    else if(method.find(DELETE)==0)
        handle_delete();
-    else if(command.find(POST)==0)
-        handle_post();
+    else if(method.find(POST)==0)
+        handle_post(method);
     else {
         cout<<"Bad Request"<<endl;
     }
        
     }
+}
+
+void Utaste::handle_post(const string method) {
+    int state;
+     std::string command, secondOrder;
+    std::istringstream iss(method);
+    iss >> command >> secondOrder;
+    
+    if (secondOrder == "signup") {
+         std::string username, password;
+        std::string word;
+    
+    while (iss >> word) {
+        if (word == "username") {
+            iss >> word;
+            username = word.substr(1, word.size() - 2); // Remove the quotes
+        } else if (word == "password") {
+            iss >> word;
+            password = word.substr(1, word.size() - 2); // Remove the quotes
+        }
+    }
+
+    std::cout << "Username: " << username << std::endl;
+    std::cout << "Password: " << password << std::endl;
+    }
+
+   
+
 }
