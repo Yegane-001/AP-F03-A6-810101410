4ca1d5d68c2374bf6e46389ebe9e163658101037
Yegane-001 <yeganeheidari435@gmail.com>
2024-12-16 22:12:21 +0330

    Utaste class added. This class will be the main class of the program.

---
+++ Utaste.h
@@ -0,0 +1,18 @@
+#ifndef UTASTE_H
+#define UTASTE_H
+using namespace std;
+#include <string>
+
+class Utaste {
+private:
+    
+public:
+   void handle_input();
+   void handle_get();
+   void handle_put();
+   void handle_delete();
+   void handle_post();
+
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <string>
+#include <vector>
+#include "Utaste.h"
+
+int main() {
+    Utaste utaste;
+    utaste.handle_input();
+}
