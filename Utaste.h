0d894a072e4828a1f724f34c89743dcb2ddff086
Yegane-001 <yeganeheidari435@gmail.com>
2024-12-25 20:01:02 +0330

    const string command for get_put added

--- Utaste.h
+++ Utaste.h
@@ -4,16 +4,19 @@ using namespace std;
 #include <string>
 #include <vector>
 #include "User.h"
+#include "District.h"
 
 class Utaste {
 private:
     vector<User> users;
     User* current_user = nullptr;
+    vector<District> districts;
 
 public:
+    Utaste(const vector<District>& districts);
     void handle_input();
-    void handle_get();
-    void handle_put();
+    void handle_get(const string command);
+    void handle_put(const string command);
     void handle_delete();
     void handle_post(const string command);
     User* find_user(const string& username);
