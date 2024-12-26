b910af45a1e278c32be4a22f6bd78e1c752b28a6
Yegane-001 <yeganeheidari435@gmail.com>
2024-12-26 15:43:05 +0330

    vector of restaurants added to Utaste arguments

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 #include "Utaste.h"
+#include "District.h"
+#include "Restaurant.h"
+#include "Readfiles.h"
+using namespace std;
 
-int main() {
-    Utaste utaste;
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cerr << "Usage: " << argv[0] << " <districts_file> <restaurants_file>" << endl;
+        return 1;
+    }
+    string districtsFile = argv[1];
+    string restaurantsFile = argv[2];
+    vector<District> districts = readDistricts(districtsFile);
+    vector<Restaurant> restaurants = readRestaurants(restaurantsFile);
+    Utaste utaste(districts,restaurants);
+    //cout << "\nRestaurants Data:\n";
+    //for (const auto& r : restaurants) r.print();
     utaste.handle_input();
+
+    return 0;
 }
