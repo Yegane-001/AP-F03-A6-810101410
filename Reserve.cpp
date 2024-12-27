0866c603353d2858812d07043e14e9c494d3ac16
Yegane-001 <yeganeheidari435@gmail.com>
2024-12-27 03:50:16 +0330

    the funv to get food vector added

--- Reserve.cpp
+++ Reserve.cpp
@@ -1,42 +1,27 @@
 #include "Reserve.h"
 #include <iostream>
 using namespace std;
-
-// سازنده کلاس
 #include "Reserve.h"
 #include <iostream>
 
-Reserve::Reserve(Restaurant& r, User& u, int id, int tbl, int start_time, int end_time)
-    : restaurant(r), user(u), reserve_id(id), table(tbl) {
+Reserve::Reserve(Restaurant& r, User& u, int id, int tbl, int start_time, int end_time, const vector<string>& foods)
+    : restaurant(r), user(u), reserve_id(id), table(tbl), foodList(foods){
     reserved_time[start_time] = end_time;
 }
 
-
-// متد دریافت شناسه رزرو
 int Reserve::get_reserveId() const {
     return reserve_id;
 }
 
-// متد دریافت شماره میز
 int Reserve::get_table() const {
     return table;
 }
 
-// متد دریافت زمان‌های رزرو
 map<int, int> Reserve::get_reservedTime() const {
     return reserved_time;
 }
 
-// متد چاپ جزئیات رزرو
-/*void Reserve::printReservationDetails() const {
-    cout << "Reservation Details:" << endl;
-    cout << "Reserve ID: " << reserve_id << endl;
-    cout << "User: " << user.get_username() << endl;
-    cout << "Restaurant: " << restaurant.restaurantName << endl;
-    cout << "Table: " << table << endl;
-    cout << "Reserved Time: ";
-    for (const auto& time : reserved_time) {
-        cout << time.first << ":" << time.second << " ";
-    }
-    cout << endl;
-}*/
+const vector<string>& Reserve::get_foodList() const {
+     return foodList; 
+}
+
