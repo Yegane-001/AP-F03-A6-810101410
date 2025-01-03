f0393b562a6ea9a1ddb80198d2bb7eab31250959
Yegane-001 <yeganeheidari435@gmail.com>
2025-01-03 14:19:39 +0330

    datas abount discount added to restaurant datas

--- main.cpp
+++ main.cpp
@@ -7,17 +7,35 @@
 #include "District.h"
 #include "Restaurant.h"
 #include "Readfiles.h"
+#include "Discount.h"
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        cerr << "Usage: " << argv[0] << " <districts_file> <restaurants_file>" << endl;
+    if (argc != 4) {
+        cerr << "Usage: " << argv[0] << " <districts_file> <restaurants_file> <discounts_file>" << endl;
         return 1;
     }
+
     string districtsFile = argv[1];
     string restaurantsFile = argv[2];
+    string discountsFile = argv[3];
     vector<District> districts = readDistricts(districtsFile);
     vector<Restaurant> restaurants = readRestaurants(restaurantsFile);
+    vector<pair<string, vector<Discount*>>> discounts = readDiscounts(discountsFile);
+    for (auto& restaurant : restaurants) {
+        for (const auto& discount_pair : discounts) {
+            if (restaurant.restaurantName == discount_pair.first) {
+                restaurant.discounts = discount_pair.second;
+            }
+        }
+    }
+    for (const auto& restaurant : restaurants) {
+        restaurant.print();
+        cout << "Discounts:\n";
+        for (const auto& discount : restaurant.discounts) {
+            cout << "Type: " << discount->get_type() << ", Value: " << discount->get_value() << "\n";
+        }
+    }
     Utaste utaste(districts,restaurants);
     utaste.handle_input();
 
