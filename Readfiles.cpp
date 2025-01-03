86675d76ca1ea57e0c059de3f914ac3c8851ba75
Yegane-001 <yeganeheidari435@gmail.com>
2025-01-03 14:17:59 +0330

    a function to read discounts datas added

--- Readfiles.cpp
+++ Readfiles.cpp
@@ -27,6 +27,7 @@ vector<District> readDistricts(const string& filePath) {
 }
 
 vector<Restaurant> readRestaurants(const string& filePath) {
+
     vector<Restaurant> restaurants;
     ifstream file(filePath);
     string line, token;
@@ -64,4 +65,62 @@ vector<Restaurant> readRestaurants(const string& filePath) {
         restaurants.push_back(r);
     }
     return restaurants;
+}
+
+vector<pair<string, vector<Discount*>>> readDiscounts(string filename) {
+    ifstream file(filename);
+    vector<pair<string, vector<Discount*>>> restaurant_discounts;
+
+    if (!file.is_open()) {
+        throw runtime_error("Could not open discounts file.");
+    }
+
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string restaurant_name, total_price_discount, first_order_discount, food_discounts;
+        getline(ss, restaurant_name, ',');
+        getline(ss, total_price_discount, ',');
+        getline(ss, first_order_discount, ',');
+        getline(ss, food_discounts, ',');
+
+        vector<Discount*> discounts;
+
+        if (total_price_discount != "none") {
+            stringstream tp_stream(total_price_discount);
+            string type;
+            int minimum, value;
+            getline(tp_stream, type, ';');
+            tp_stream >> minimum >> value;
+            discounts.push_back(new Total_Price_Discount(type, value, minimum));
+        }
+
+        if (first_order_discount != "none") {
+            stringstream fo_stream(first_order_discount);
+            string type;
+            int value;
+            getline(fo_stream, type, ';');
+            fo_stream >> value;
+            discounts.push_back(new First_Order_Discount(type, value));
+        }
+
+        if (food_discounts != "none") {
+            stringstream fd_stream(food_discounts);
+            string token;
+            while (getline(fd_stream, token, '/')) {
+                stringstream food_stream(token);
+                string type, food_name;
+                int value;
+                getline(food_stream, type, ':');
+                getline(food_stream, food_name, ':');
+                food_stream >> value;
+                discounts.push_back(new Food_Discount(type, value, food_name));
+            }
+        }
+
+        restaurant_discounts.push_back({restaurant_name, discounts});
+    }
+
+    file.close();
+    return restaurant_discounts;
 }
--- Readfiles.h
+++ Readfiles.h
@@ -3,13 +3,17 @@
 
 #include "District.h"
 #include "Restaurant.h"
+#include "Discount.h"
 #include <fstream>
 #include <sstream>
 #include <vector>
 #include <iostream>
+#include <map>
+#include <string>
 using namespace std;
 
 vector<Restaurant> readRestaurants(const string& filePath);
 vector<District> readDistricts(const string& filePath);
+vector<pair<string, vector<Discount*>>> readDiscounts(string filename);
 
 #endif
